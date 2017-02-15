77e7722b621014ee2fcd39fa7249e443334eb9b0
Igor Chernyakov <i.g.chernyakov@yandex.ru>
2017-02-15 23:18:49 +0700

    It works now!

--- adraft/urls.py
+++ adraft/urls.py
@@ -4,10 +4,12 @@
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('base.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
