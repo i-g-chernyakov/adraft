43df083c1ceecbb4603d010418d0a3fe2dfb24ee
i.g.chernyakov <i.g.chernyakov@yandex.ru>
2017-02-20 15:56:19 +0700

    settings changed

--- adraft/settings/base.py
+++ adraft/settings/base.py
@@ -81,3 +81,18 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
+
+DATABASES = {
+    'default': {
+        'ENGINE': os.environ.get('ADRAFT_DATABASE_ENGINE', 'django.db.backends.postgresql_psycopg2'),
+        # 'django.db.backends.mysql'
+        'NAME': os.environ.get('ADRAFT_DATABASE_NAME', ''),
+        'USER': os.environ.get('ADRAFT_DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('ADRAFT_DATABASE_PASSWORD', ''),
+        'HOST': os.environ.get('ADRAFT_DATABASE_HOST', ''),
+        'PORT': os.environ.get('ADRAFT_DATABASE_PORT', ''),
+    }
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- adraft/settings/production.py
+++ adraft/settings/production.py
@@ -1,20 +1,5 @@
 from adraft.settings.base import *
 
 DEBUG = False
+
 ALLOWED_HOSTS = ['adraft.pythonanywhere.com']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('ADRAFT_DATABASE_NAME', ''),
-        'USER': os.environ.get('ADRAFT_DATABASE_USER', ''),
-        'PASSWORD': os.environ.get('ADRAFT_DATABASE_PASSWORD', ''),
-        'HOST': os.environ.get('ADRAFT_DATABASE_HOST', ''),
-        'PORT': os.environ.get('ADRAFT_DATABASE_PORT', ''),
-    }
-}
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-
-
